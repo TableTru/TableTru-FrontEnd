fa029424fcc22710df2d84e07ce52eb92b8a5f8c
aungpor <napat.patc@ku.th>
2024-01-16 13:59:37 +0700

    update

---
+++ src/app/profile/page.tsx
@@ -0,0 +1,17 @@
+"use client";
+
+import React, { useEffect, useState } from "react";
+
+export default function Profile() {
+    
+    useEffect((() => {
+    }),[])
+
+    return (
+      <>
+        <div>
+          <h1>หน้า Profile จ้า</h1>
+        </div>
+      </>
+    );
+  }
