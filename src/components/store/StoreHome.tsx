329c95e39cc9466dae2c5dd8f4bc6780578e32a5
aungpor <napat.patc@ku.th>
2024-01-29 14:54:14 +0700

    update

--- src/components/store/StoreHome.tsx
+++ src/components/store/StoreHome.tsx
@@ -23,7 +23,7 @@ import {
     AccordionDetails,
     Autocomplete,
     Card,
-    CardContent ,
+    CardContent,
     CardMedia,
 
 
@@ -33,6 +33,9 @@ import IconButton from '@mui/material/IconButton';
 import SkipPreviousIcon from '@mui/icons-material/SkipPrevious';
 import PlayArrowIcon from '@mui/icons-material/PlayArrow';
 import SkipNextIcon from '@mui/icons-material/SkipNext';
+import ListItemIcon from '@mui/material/ListItemIcon';
+import DraftsIcon from '@mui/icons-material/Drafts';
+import InboxIcon from '@mui/icons-material/Inbox';
 import { createTheme, ThemeProvider, useTheme } from '@mui/material/styles';
 
 
@@ -46,34 +49,65 @@ export default function StoreHome() {
                     <CssBaseline />
 
                     <Box sx={{ marginTop: 8, display: 'flex', alignItems: 'left', }}>
-                        <Card sx={{ display: 'flex', width: '100%' }}>
-                            <CardMedia
-                                component="img"
-                                sx={{ width: 151 }}
-                                image="https://pbs.twimg.com/media/FXTTYWfVUAAjIph?format=png&name=medium"
-                                alt="Live from space album cover"
-                            />
+                        <Grid container spacing={2}>
+                            <Grid item xs={12}>
+                                <Card sx={{ display: 'flex', width: '100%' }}>
+                                    <CardMedia
+                                        component="img"
+                                        sx={{ width: 151 }}
+                                        image="https://pbs.twimg.com/media/FXTTYWfVUAAjIph?format=png&name=medium"
+                                        alt="Live from space album cover"
+                                    />
 
-                            <Box sx={{ display: 'flex', flexDirection: 'column' }}>
-                                <CardContent sx={{ flex: '1 0 auto' }}>
-                                    <Typography component="div" variant="h5">
-                                        ร้านอาหารของฉัน
-                                    </Typography>
-                                </CardContent>
-                                <Box sx={{ display: 'flex', alignItems: 'center', pl: 1, pb: 1 }}>
-                                    <IconButton aria-label="previous">
-                                        {theme.direction === 'rtl' ? <SkipNextIcon /> : <SkipPreviousIcon />}
-                                    </IconButton>
-                                    <IconButton aria-label="play/pause">
-                                        <PlayArrowIcon sx={{ height: 38, width: 38 }} />
-                                    </IconButton>
-                                    <IconButton aria-label="next">
-                                        {theme.direction === 'rtl' ? <SkipPreviousIcon /> : <SkipNextIcon />}
-                                    </IconButton>
-                                </Box>
-                            </Box>
+                                    <Box sx={{ display: 'flex', flexDirection: 'column' }}>
+                                        <CardContent sx={{ flex: '1 0 auto' }}>
+                                            <Typography component="div" variant="h5">
+                                                ร้านอาหารของฉัน
+                                            </Typography>
+                                        </CardContent>
+                                        <Box sx={{ display: 'flex', alignItems: 'center', pl: 1, pb: 1 }}>
+                                            <IconButton aria-label="previous">
+                                                {theme.direction === 'rtl' ? <SkipNextIcon /> : <SkipPreviousIcon />}
+                                            </IconButton>
+                                            <IconButton aria-label="play/pause">
+                                                <PlayArrowIcon sx={{ height: 38, width: 38 }} />
+                                            </IconButton>
+                                            <IconButton aria-label="next">
+                                                {theme.direction === 'rtl' ? <SkipPreviousIcon /> : <SkipNextIcon />}
+                                            </IconButton>
+                                        </Box>
+                                    </Box>
 
-                        </Card>
+                                </Card>
+                            </Grid>
+
+                            <Grid item xs={12}>
+                                <List>
+                                    <ListItem disablePadding>
+                                        <ListItemButton>
+                                            <ListItemIcon>
+                                                <InboxIcon />
+                                            </ListItemIcon>
+                                            <ListItemText primary="แก้ไขรายละเอียด" />
+                                        </ListItemButton>
+                                    </ListItem>
+                                    <ListItem disablePadding>
+                                        <ListItemButton>
+                                            <ListItemIcon>
+                                                <DraftsIcon />
+                                            </ListItemIcon>
+                                            <ListItemText primary="Drafts" />
+                                        </ListItemButton>
+                                    </ListItem>
+                                </List>
+                            </Grid>
+
+                        </Grid>
+
+
+                        <nav aria-label="main mailbox folders">
+
+                        </nav>
                     </Box>
 
                 </Container>
