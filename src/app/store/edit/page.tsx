90d733fda998740d513f1fb41ecd3c8d7bd1b8d6
aungpor <napat.patc@ku.th>
2024-01-29 15:16:34 +0700

    update

---
+++ src/app/store/edit/page.tsx
@@ -0,0 +1,13 @@
+import StoreHome from '@/components/store/StoreHome'
+
+function App() {
+    return (
+        <>
+        <div className="bg-zinc-50 min-h-full">
+            <h1>Edit Store</h1>
+        </div>
+        </>
+    );
+
+}
+export default App;
