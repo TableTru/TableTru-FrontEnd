e417a2440c4d552079705bedb0a8fe859a070a5c
KenzieLeonic <57589336+KenzieLeonic@users.noreply.github.com>
2024-02-04 15:31:16 +0700

    add search and filter in all product

--- src/app/products/page.tsx
+++ src/app/products/page.tsx
@@ -1,11 +1,27 @@
 import RestaurantCard from "@/components/cards/Restaurantcard"
 import Appbar from "@/components/Appbar"
+import SearchFilterBar from "@/components/SearchHeader"
+import SearchBar from "@/components/SearchBar"
+import FilterBar from "@/components/FilterBar"
+
 const FoodDetail = () => {
 
     return (
         <>
         <Appbar/>
-
+        <div className="py-20 px-2">
+        <div className="max-w-md mx-auto rounded-lg overflow-hidden md:max-w-xl">
+          <div className="md:flex">
+            <div className="w-full p-3">
+              <div className="relative flex items-center ">
+               <SearchBar/>
+               <FilterBar/>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+        
         <section   
         className="w-fit flex mx-auto grid grid-cols-1 lg:grid-cols-3 md:grid-cols-2 justify-items-center justify-center gap-y-20 gap-x-14 mt-10 mb-5">
             <RestaurantCard />
---
+++ src/components/FilterBar.tsx
@@ -0,0 +1,103 @@
+"use client";
+
+import * as React from 'react';
+import Box from '@mui/material/Box';
+import SwipeableDrawer from '@mui/material/SwipeableDrawer';
+import Button from '@mui/material/Button';
+import List from '@mui/material/List';
+import Divider from '@mui/material/Divider';
+import ListItem from '@mui/material/ListItem';
+import ListItemButton from '@mui/material/ListItemButton';
+import ListItemIcon from '@mui/material/ListItemIcon';
+import ListItemText from '@mui/material/ListItemText';
+import InboxIcon from '@mui/icons-material/MoveToInbox';
+import MailIcon from '@mui/icons-material/Mail';
+import { spacing } from '@mui/system';
+import IconButton from '@mui/material/IconButton';
+import FilterListIcon from '@mui/icons-material/FilterList';
+import FilterList from '@mui/icons-material/FilterList';
+import Radio from '@mui/material/Radio';
+import RadioGroup from '@mui/material/RadioGroup';
+import FormControlLabel from '@mui/material/FormControlLabel';
+import FormControl from '@mui/material/FormControl';
+import FormLabel from '@mui/material/FormLabel';
+
+
+type Anchor = 'right';
+
+export default function SwipeableTemporaryDrawer() {
+  const [state, setState] = React.useState({
+    right: false,
+  });
+
+  const toggleDrawer =
+    (anchor: Anchor, open: boolean) =>
+    (event: React.KeyboardEvent | React.MouseEvent) => {
+      if (
+        event &&
+        event.type === 'keydown' &&
+        ((event as React.KeyboardEvent).key === 'Tab' ||
+          (event as React.KeyboardEvent).key === 'Shift')
+      ) {
+        return;
+      }
+
+      setState({ ...state, [anchor]: open });
+    };
+
+  const list = (anchor: Anchor) => (
+    <Box
+      sx={{ width: anchor === 'top'  ? 'auto' : 250 }}
+      role="presentation"
+      onClick={toggleDrawer(anchor, false)}
+      onKeyDown={toggleDrawer(anchor, false)}
+    >
+      <List>
+        {['Inbox', 'Starred', 'Send email', 'Drafts'].map((text, index) => (
+          <ListItem key={text} disablePadding>
+            <ListItemButton>
+              <ListItemIcon>
+                {index % 2 === 0 ? <InboxIcon /> : <MailIcon />}
+              </ListItemIcon>
+              <ListItemText primary={text} />
+            </ListItemButton>
+          </ListItem>
+        ))}
+      </List>
+      <Divider />
+
+      <FormControl sx={ {p:"16px"} }>
+      <FormLabel id="demo-radio-buttons-group-label">Gender</FormLabel>
+      <RadioGroup
+        aria-labelledby="demo-radio-buttons-group-label"
+        defaultValue="female"
+        name="radio-buttons-group"
+      >
+        <FormControlLabel value="female" control={<Radio />} label="Female" />
+        <FormControlLabel value="male" control={<Radio />} label="Male" />
+        <FormControlLabel value="other" control={<Radio />} label="Other" />
+      </RadioGroup>
+    </FormControl>
+
+
+    </Box>
+  );
+
+  return (
+    <div>
+      {(['right'] as const).map((anchor) => (
+        <React.Fragment key={anchor}>
+          <IconButton size="large" onClick={toggleDrawer(anchor, true)} sx={{ ml: "16px"}} aria-label="FilterListIcon"> <FilterList color="primary"/> </IconButton>
+          <SwipeableDrawer
+            anchor={anchor}
+            open={state[anchor]}
+            onClose={toggleDrawer(anchor, false)}
+            onOpen={toggleDrawer(anchor, true)}
+          >
+            {list(anchor)}
+          </SwipeableDrawer>
+        </React.Fragment>
+      ))}
+    </div>
+  );
+}
