b562a97cd22dacb7789fa333943d28990702b925
aungpor <napat.patc@ku.th>
2024-02-14 15:40:13 +0700

    update store create

--- src/components/profile/ProfileCard.tsx
+++ src/components/profile/ProfileCard.tsx
@@ -18,6 +18,8 @@ import {
     ListItemAvatar,
     ListItemText,
     ListItemButton,
+    Chip,
+    Stack,
 
 
 } from "@mui/material";
@@ -26,6 +28,7 @@ import ImageIcon from '@mui/icons-material/Image';
 import WorkIcon from '@mui/icons-material/Work';
 import BeachAccessIcon from '@mui/icons-material/BeachAccess';
 import { createTheme, ThemeProvider } from '@mui/material/styles';
+import ArrowForwardIcon from '@mui/icons-material/ArrowForward';
 
 export default function ProfileCard() {
     const [userData, setUserData] = useState();
@@ -58,103 +61,113 @@ export default function ProfileCard() {
         <ThemeProvider theme={createTheme()}>
             <Container component="main" maxWidth="xs">
                 <CssBaseline />
-                <Box
-                    sx={{
-                        marginTop: 8,
-                        display: 'flex',
-                        flexDirection: 'column',
-                        alignItems: 'center',
-                    }}
-                >
-                    <Avatar src="https://pbs.twimg.com/media/FXTTYWfVUAAjIph?format=png&name=medium" sx={{ width: 100, height: 100, m: 1, bgcolor: 'secondary.main' }} />
-                    <Typography component="h1" variant="h6">
-                        Aungpor
-                    </Typography>
+
+                <Box sx={{ marginTop: 8, marginBottom: 8, display: 'flex', flexDirection: 'column', alignItems: 'left', }}>
+                    <Box
+                        sx={{
+                            display: 'flex',
+                            flexDirection: 'column',
+                            alignItems: 'center',
+                        }}
+                    >
+                        <Avatar src="https://pbs.twimg.com/media/FXTTYWfVUAAjIph?format=png&name=medium" sx={{ width: 100, height: 100, m: 1, bgcolor: 'secondary.main' }} />
+                        <Typography component="h1" variant="h6">
+                            Aungpor
+                        </Typography>
+                    </Box>
+
+                    <Box
+                        sx={{
+                            marginTop: 2,
+                            display: 'flex',
+                            flexDirection: 'column',
+                            alignItems: 'left',
+                        }}
+                    >
+                        <Typography component="h1" variant="h6">
+                            Email:
+                        </Typography>
+                        <Typography component="h1" variant="h6">
+                            Phone Number:
+                        </Typography>
+                    </Box>
+
+                    <Box
+                        sx={{
+                            marginTop: 2,
+                            display: 'flex',
+                            flexDirection: 'column',
+                        }}
+                    >
+                        <Link href="/profile/edit" sx={{
+                            display: 'flex',
+                            flexDirection: 'column',
+                        }}>
+                            <Button variant="outlined">แก้ไขโปรไฟล์</Button>
+                        </Link>
+
+                    </Box>
+
+                    <Box
+                        sx={{
+                            marginTop: 4,
+                            display: 'flex',
+                            flexDirection: 'column',
+                            alignItems: 'left',
+                        }}
+                    >
+                        <Typography component="h1" variant="h6">
+                            ตำแหน่งของฉัน:
+                        </Typography>
+                    </Box>
+
+                    <Box
+                        sx={{
+                            marginTop: 4,
+                            display: 'flex',
+                            flexDirection: 'column',
+                            alignItems: 'left',
+                        }}
+                    >
+                        <Typography component="h1" variant="h6">
+                            ร้านที่รีวิว
+                        </Typography>
+
+                        {reviewList.map((item) => (
+                            <List className="bottom-line" sx={{ width: '100%', bgcolor: 'background.paper' }}>
+                                <ListItem>
+                                    <Box sx={{ display: 'flex', flexDirection: 'column' }}>
+                                        <ListItemText primary={`${item.review}`} secondary={`จำนวน ${item.store} คน`} />
+                                        <Box sx={{ marginTop: 1, display: 'flex', flexDirection: 'column' }}>
+                                            <a href="/">
+                                                <p className="activity">กดดูรายละเอียด<ArrowForwardIcon /></p>
+                                            </a>
+                                        </Box>
+                                    </Box>
+
+                                </ListItem>
+                            </List>
+                        ))}
+
+                        <Link href="/store/create" sx={{
+                            marginBottom: 2,
+                            display: 'flex',
+                            flexDirection: 'column'
+                        }}>
+                            <Button variant="outlined">ลงทะเบียนร้านค้า</Button>
+                        </Link>
+                        <Link href="/" sx={{
+                            marginBottom: 2,
+                            display: 'flex',
+                            flexDirection: 'column'
+                        }}>
+                            <Button variant="contained" className={"bg-red-700"} color="error" sx={{ marginBottom: 2 }} onClick={logout}>Logout</Button>
+                        </Link>
+
+                    </Box>
                 </Box>
 
-                <Box
-                    sx={{
-                        marginTop: 2,
-                        display: 'flex',
-                        flexDirection: 'column',
-                        alignItems: 'left',
-                    }}
-                >
-                    <Typography component="h1" variant="h6">
-                        Email:
-                    </Typography>
-                    <Typography component="h1" variant="h6">
-                        Phone Number:
-                    </Typography>
-                </Box>
 
-                <Box
-                    sx={{
-                        marginTop: 2,
-                        display: 'flex',
-                        flexDirection: 'column',
-                    }}
-                >
-                    <Link href="/profile/edit" sx={{
-                        display: 'flex',
-                        flexDirection: 'column',
-                    }}>
-                        <Button variant="outlined">แก้ไขโปรไฟล์</Button>
-                    </Link>
-                    
-                </Box>
-
-                <Box
-                    sx={{
-                        marginTop: 4,
-                        display: 'flex',
-                        flexDirection: 'column',
-                        alignItems: 'left',
-                    }}
-                >
-                    <Typography component="h1" variant="h6">
-                        ตำแหน่งของฉัน:
-                    </Typography>
-                </Box>
-
-                <Box
-                    sx={{
-                        marginTop: 4,
-                        display: 'flex',
-                        flexDirection: 'column',
-                        alignItems: 'left',
-                    }}
-                >
-                    <Typography component="h1" variant="h6">
-                        ร้านที่รีวิว
-                    </Typography>
-
-                    {reviewList.map((item) => (
-                        <List sx={{ width: '100%', bgcolor: 'background.paper' }}>
-                            <ListItem>
-                                <ListItemButton>
-                                    <ListItemText primary={item.review} secondary={item.store} />
-                                </ListItemButton>
-                            </ListItem>
-                        </List>
-                    ))}
-
-                    <Link href="/profile/edit" sx={{
-                        marginBottom: 2,
-                        display: 'flex',
-                        flexDirection: 'column'
-                    }}>
-                        <Button variant="outlined">ลงทะเบียนร้านค้า</Button>
-                    </Link>
-                    <Link href="/" sx={{
-                        marginBottom: 2,
-                        display: 'flex',
-                        flexDirection: 'column'
-                    }}>
-                        <Button variant="contained" className={"bg-red-700"} color="error" sx={{ marginBottom: 2 }} onClick={logout}>Logout</Button>
-                    </Link>
-                    
-                </Box>
 
             </Container>
 
