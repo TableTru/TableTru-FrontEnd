72e9cd46fdacb3cf4393a392ed2547f21d900f24
KenzieLeonic <57589336+KenzieLeonic@users.noreply.github.com>
2024-02-15 11:45:02 +0700

    edit sign in

--- src/app/(auth)/register/page.tsx
+++ src/app/(auth)/register/page.tsx
@@ -3,7 +3,7 @@ import RegisterForm from "@/components/login/RegisterForm"
 
 const App = () => {
     return (
-        <div className="min-h-screen">
+        <div className="min-h-screen bg-zinc-50 pt-8 space-x-4">
             <RegisterForm />
         </div>
 
