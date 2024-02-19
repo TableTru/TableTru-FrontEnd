b102adb271009e4b70ffdf5c6318640ba11ff66d
KenzieLeonic <57589336+KenzieLeonic@users.noreply.github.com>
2024-02-19 23:16:48 +0700

    edit confirm

--- src/components/botton/ConfirmButton.tsx
+++ src/components/botton/ConfirmButton.tsx
@@ -29,72 +29,45 @@ export default function ConfirmButton () {
     }
     const ref = React.useRef(null);
 
-    function information(){
-        return (
-            <>
-                <ul className="max-w-md space-y-1 text-gray-500 list-inside dark:text-gray-400">
-                    <li className="flex items-center">
-                        <svg class="w-3.5 h-3.5 me-2 text-green-500 dark:text-green-400 flex-shrink-0" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="currentColor" viewBox="0 0 20 20">
-                            <path d="M10 .5a9.5 9.5 0 1 0 9.5 9.5A9.51 9.51 0 0 0 10 .5Zm3.707 8.207-4 4a1 1 0 0 1-1.414 0l-2-2a1 1 0 0 1 1.414-1.414L9 10.586l3.293-3.293a1 1 0 0 1 1.414 1.414Z"/>
-                        </svg>
-                        At least 10 characters
-                    </li>
-                    <li className="flex items-center">
-                        <svg class="w-3.5 h-3.5 me-2 text-green-500 dark:text-green-400 flex-shrink-0" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="currentColor" viewBox="0 0 20 20">
-                            <path d="M10 .5a9.5 9.5 0 1 0 9.5 9.5A9.51 9.51 0 0 0 10 .5Zm3.707 8.207-4 4a1 1 0 0 1-1.414 0l-2-2a1 1 0 0 1 1.414-1.414L9 10.586l3.293-3.293a1 1 0 0 1 1.414 1.414Z"/>
-                        </svg>
-                        At least one lowercase character
-                    </li>
-                    <li className="flex items-center">
-                        <svg class="w-3.5 h-3.5 me-2 text-gray-500 dark:text-gray-400 flex-shrink-0" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="currentColor" viewBox="0 0 20 20">
-                            <path d="M10 .5a9.5 9.5 0 1 0 9.5 9.5A9.51 9.51 0 0 0 10 .5Zm3.707 8.207-4 4a1 1 0 0 1-1.414 0l-2-2a1 1 0 0 1 1.414-1.414L9 10.586l3.293-3.293a1 1 0 0 1 1.414 1.414Z"/>
-                        </svg>
-                        At least one special character, e.g., ! @ # ?
-                    </li>
-                </ul>
-
-            </>
-
-        );
-    }
-
-
     function handleButtonConfirm() {
         Swal.fire<data>({
-            title: "โปรดตรวจสอบรายละเอียดการจอง",
-            text: "หากต้องการแก้ไข โปรดติดต่อสาขาที่ทำการจองร้านอาหารไว้",
+            title: "แน่ใจหรือว่าจะยืนยัน",
+            text: "โปรดตรวจสอบรายละเอียดการจอง",
             icon: "warning",
-            html: `
-
-    <input type="text" ref={ref} id="username"  class="swal2-input" placeholder="Username" onChange={handleChange}>
-    <input type="text" ref={ref}  id="phoneNumber" class="swal2-input" placeholder="Phone Number" onChange={handleChange}  >
-  `,
-            didOpen: () => {
-                const popup = Swal.getPopup()!
-                usernameInput = popup.querySelector('#username') as HTMLInputElement
-                phoneInput = popup.querySelector('#phoneNumber') as HTMLInputElement
-                usernameInput.onkeyup = (event) => event.key === 'Enter' && Swal.clickConfirm()
-                phoneInput.onkeyup = (event) => event.key === 'Enter' && Swal.clickConfirm()
-            },
             showCancelButton: true,
             confirmButtonText: "ยืนยัน",
             confirmButtonColor: "#0E9F6E",
             cancelButtonText: "ย้อนกลับ",
             reverseButtons: true,
-            preConfirm: () => {
-                const username = usernameInput.value
-                const phoneNumber = phoneInput.value
-                if (!username || !phoneNumber) {
-                    Swal.showValidationMessage(`Please enter username and phone number`)
-                }
-                return { username, phoneNumber }
-            }
+  //           html: `
+  //
+  //   <input type="text" ref={ref} id="username"  class="swal2-input" placeholder="Username" onChange={handleChange}>
+  //   <input type="text" ref={ref}  id="phoneNumber" class="swal2-input" placeholder="Phone Number" onChange={handleChange}  >
+  // `,
+  //           didOpen: () => {
+  //               const popup = Swal.getPopup()!
+  //               usernameInput = popup.querySelector('#username') as HTMLInputElement
+  //               phoneInput = popup.querySelector('#phoneNumber') as HTMLInputElement
+  //               usernameInput.onkeyup = (event) => event.key === 'Enter' && Swal.clickConfirm()
+  //               phoneInput.onkeyup = (event) => event.key === 'Enter' && Swal.clickConfirm()
+  //           },
+
+  //           preConfirm: () => {
+  //               const username = usernameInput.value
+  //               const phoneNumber = phoneInput.value
+  //               if (!username || !phoneNumber) {
+  //                   Swal.showValidationMessage(`Please enter username and phone number`)
+  //               }
+  //               return { username, phoneNumber }
+  //           }
         }).then((result) => {
             if (result.isConfirmed) {
                 Swal.fire({
                     title: "ยืนยันสำเร็จ",
                     text: "",
-                    icon: "success"
+                    icon: "success",
+                    confirmButtonText: "ตกลง",
+                    confirmButtonColor: "#0E9F6E",
                 });
             }
         })
