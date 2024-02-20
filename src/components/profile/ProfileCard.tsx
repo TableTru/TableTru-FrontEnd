78121037372c763ff72336e2830131ad042a6643
aungpor <napat.patc@ku.th>
2024-02-20 16:36:38 +0700

    tempLogin and profileCard

--- src/components/profile/ProfileCard.tsx
+++ src/components/profile/ProfileCard.tsx
@@ -1,6 +1,6 @@
 "use client";
 
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import Image from "next/image";
 import {
     Tabs,
@@ -29,38 +29,107 @@ import WorkIcon from '@mui/icons-material/Work';
 import BeachAccessIcon from '@mui/icons-material/BeachAccess';
 import { createTheme, ThemeProvider } from '@mui/material/styles';
 import ArrowForwardIcon from '@mui/icons-material/ArrowForward';
-import AppBar from '@/components/HeaderAppBar'
+
+interface User {
+    user_id: number;
+    username: string;
+    password: string;
+    profile_image: string;
+    user_status: string;
+    email: string;
+    phone_num: string;
+    latitude: number;
+    longitude: number;
+    createAt: Date;
+    updateAt: Date;
+}
+
+interface Review {
+    store_id: number;
+    store_name: string;
+    review_comment: string;
+    createAt: Date;
+    updateAt: Date;
+}
+
+const userTemp: User =
+{
+    user_id: 1,
+    username: "Aungpor",
+    password: "por1234",
+    user_status: "user",
+    profile_image: "https://pbs.twimg.com/media/FXTTYWfVUAAjIph?format=png&name=medium",
+    email: "aungpor.napat@gmail.com",
+    phone_num: "0813111234",
+    latitude: 0,
+    longitude: 0,
+    createAt: new Date(),
+    updateAt: new Date(),
+
+}
+
+const reviewTemp: Review[] = [
+    {
+        store_id: 1,
+        store_name: "ร้าน1",
+        review_comment: "ข้าวหมูกรอบอร่อยมาก",
+        createAt: new Date(),
+        updateAt: new Date()
+    },
+    {
+        store_id: 2,
+        store_name: "ร้าน2",
+        review_comment: "สู่ความเวิ้งว้างอันไกลโพ้นนนนนนนนนนนนนนนนน",
+        createAt: new Date(),
+        updateAt: new Date()
+    },
+]
 
 export default function ProfileCard() {
-    const [userData, setUserData] = useState();
+    const [userData, setUserData] = useState<User>();
+    const [reviewData, setReviewData] = useState<Review[]>([])
 
-    const reviewList = [
-        {
-            review: "ข้าวหมูกรอบอร่อยมาก",
-            store: "ร้าน1",
-        },
-        {
-            review: "ข้าวหมูกรอบอร่อยมาก",
-            store: "ร้าน2",
-        },
-        {
-            review: "ข้าวหมูกรอบอร่อยมาก",
-            store: "ร้าน3",
-        },
-        {
-            review: "ข้าวหมูกรอบอร่อยมาก",
-            store: "ร้าน4",
-        },
-    ];
+    const fetchData = async () => {
+        // const data = await getUser();
+        // console.log(data);
+
+        // if (data) {
+        //     setUserData(data);
+        //     console.log(data);
+        // }
+
+        // const reviewArray = [];
+        // const userReviews = await getUserReview();
+        // console.log(userReviews);
+
+        // if (userReviews) {
+        //     for (const reviewObject of userReviews) {
+        //         reviewArray.push(reviewObject);
+        //     }
+        //     setReviewData(reviewArray);
+        //     console.log(reviewArray);
+        // }
+
+        setUserData(userTemp);
+        setReviewData(reviewTemp)
+
+    };
 
     const logout = () => {
         localStorage.removeItem('userData');
-        // window.location.replace('/')
+        window.location.replace('/')
     }
+
+    const storeCreateClick = () => {
+        window.location.replace('/store/create')
+    }
+
+    useEffect(() => {
+        fetchData();
+    }, []);
 
     return (
         <ThemeProvider theme={createTheme()}>
-            <AppBar color="primary"  className={"bg-red-700"}/>
             <Container component="main" maxWidth="xs">
                 <CssBaseline />
 
@@ -72,9 +141,9 @@ export default function ProfileCard() {
                             alignItems: 'center',
                         }}
                     >
-                        <Avatar src="https://pbs.twimg.com/media/FXTTYWfVUAAjIph?format=png&name=medium" sx={{ width: 100, height: 100, m: 1, bgcolor: 'secondary.main' }} />
+                        <Avatar src={userData?.profile_image} sx={{ width: 100, height: 100, m: 1, bgcolor: 'secondary.main' }} />
                         <Typography component="h1" variant="h6">
-                            Aungpor
+                            {userData?.username}
                         </Typography>
                     </Box>
 
@@ -82,16 +151,27 @@ export default function ProfileCard() {
                         sx={{
                             marginTop: 2,
                             display: 'flex',
-                            flexDirection: 'column',
+                            flexDirection: 'row',
                             alignItems: 'left',
                         }}
                     >
-                        <Typography component="h1" variant="h6">
-                            Email:
-                        </Typography>
-                        <Typography component="h1" variant="h6">
-                            Phone Number:
-                        </Typography>
+                        <Box sx={{ width: '20%', display: 'flex', flexDirection: 'column', alignItems: 'left' }}>
+                            <Typography component="h1" variant="h6">
+                                Email:
+                            </Typography>
+                            <Typography component="h1" variant="h6">
+                                Phone:
+                            </Typography>
+                        </Box>
+                        <Box sx={{ width: '80%', display: 'flex', flexDirection: 'column', alignItems: 'left' }}>
+                            <Typography component="h1" variant="h6">
+                                {userData?.email}
+                            </Typography>
+                            <Typography component="h1" variant="h6">
+                                {userData?.phone_num}
+                            </Typography>
+                        </Box>
+
                     </Box>
 
                     <Box
@@ -135,13 +215,13 @@ export default function ProfileCard() {
                             ร้านที่รีวิว
                         </Typography>
 
-                        {reviewList.map((item) => (
-                            <List className="bottom-line" sx={{ width: '100%', bgcolor: 'background.paper' }}>
+                        {reviewTemp.map((item, index) => (
+                            <List key={index} className="bottom-line" sx={{ width: '100%', bgcolor: 'background.paper' }}>
                                 <ListItem>
                                     <Box sx={{ display: 'flex', flexDirection: 'column' }}>
-                                        <ListItemText primary={`${item.review}`} secondary={`จำนวน ${item.store} คน`} />
+                                        <ListItemText primary={`${item.review_comment}`} secondary={`${item.store_name}`} />
                                         <Box sx={{ marginTop: 1, display: 'flex', flexDirection: 'column' }}>
-                                            <a href="/">
+                                            <a href="">
                                                 <p className="activity">กดดูรายละเอียด<ArrowForwardIcon /></p>
                                             </a>
                                         </Box>
@@ -163,7 +243,7 @@ export default function ProfileCard() {
                             display: 'flex',
                             flexDirection: 'column'
                         }}>
-                            <Button variant="contained" className={"bg-red-700"} color="error" sx={{ marginBottom: 2 }} onClick={logout}>Logout</Button>
+                            <Button variant="contained" className={"bg-red-700"} color="error" sx={{ marginBottom: 2 }} >Logout</Button>
                         </Link>
 
                     </Box>
