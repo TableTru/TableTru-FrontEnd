57308f585a7b8fc744a244db9311a29afc4c5ac9
aungpor <napat.patc@ku.th>
2024-03-07 12:53:24 +0700

    map clear

--- src/components/store/MapTest.tsx
+++ src/components/store/MapTest.tsx
@@ -9,8 +9,7 @@ const loader = new Loader({
   apiKey: process.env.NEXT_PUBLIC_GOOGLE_MAPS_API_KEY,
   version: 'weekly',
   libraries: ['places'], // เพิ่ม libraries places
-});
-const GoogleMap = () => {
+}); const GoogleMap = () => {
   const mapRef = useRef<HTMLDivElement>(null);
   const inputRef = useRef<HTMLInputElement>(null);
   const [map, setMap] = useState<google.maps.Map | null>(null);
@@ -20,20 +19,50 @@ const GoogleMap = () => {
     loader.load().then(() => {
       const google = window.google;
       if (mapRef.current && !map) {
-        setMap(
-          new google.maps.Map(mapRef.current, {
-            center: { lat: 13.7563, lng: 100.5018 }, // ตำแหน่งเริ่มต้นที่กรุงเทพมหานคร
-            zoom: 12,
-          })
-        );
+        const geolocation = navigator.geolocation;
+        if (geolocation) {
+          geolocation.getCurrentPosition(
+            (position) => {
+              const initialLocation = new google.maps.LatLng(position.coords.latitude, position.coords.longitude);
+              const newMap = new google.maps.Map(mapRef.current, {
+                  center: initialLocation,
+                  zoom: 12,
+                })
+              setMap(newMap);
+              newMap.setCenter(initialLocation);
+                new google.maps.Marker({
+                  position: initialLocation,
+                  map: newMap,
+                });
+            },
+            (error) => {
+              if (error.code === error.PERMISSION_DENIED) {
+                const newMap = new google.maps.Map(mapRef.current, {
+                  center: { lat: 13.7563, lng: 100.5018 }, // ตำแหน่งเริ่มต้นที่กรุงเทพมหานคร,
+                  zoom: 12,
+                })
+                setMap(newMap);
+                newMap.setCenter({ lat: 13.7563, lng: 100.5018 });
+                new google.maps.Marker({
+                  position: { lat: 13.7563, lng: 100.5018 },
+                  map: newMap,
+                });
+              }
+            }
+          );
+        }
+        else {
+          console.log("my location off");
+
+        }
       }
       if (inputRef.current && google) {
         const autocomplete = new google.maps.places.Autocomplete(inputRef.current);
         autocomplete.addListener('place_changed', () => {
           const place = autocomplete.getPlace();
           if (place && place.geometry && map) {
-            const location = place.geometry.location; // ตำแหน่งแบบ lat long
-            console.log(place.formatted_address); // ตำแหน่งแบบชื่อ
+            const location = place.geometry.location; //ตำแหน่งแบบ lat long
+            console.log(place.formatted_address); //ตำแหน่งแบบชื่อ
             map.setCenter(location);
             new google.maps.Marker({
               position: location,
