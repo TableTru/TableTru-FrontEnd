ddef7f0563e617089a38a69fd9fd95eb957a7eff
aungpor <napat.patc@ku.th>
2024-03-22 15:19:53 +0700

    update

--- src/app/search/[slug]/page.tsx
+++ src/app/search/[slug]/page.tsx
@@ -19,7 +19,7 @@ export default function SearchPage() {
                     <Filter />
                 </div>
                 <section className="w-fit flex mr-auto grid grid-cols-1 lg:grid-cols-3 md:grid-cols-2 justify-items-center justify-center gap-y-20 gap-x-14 mt-10 mb-5">
-                    <RestaurantCard store={storeTemp} />
+                    <RestaurantCard />
                 </section>
             </section>
         </>
