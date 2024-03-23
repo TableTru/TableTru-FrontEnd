c67d22b9d2dc9517b4a1c630252391f826f42e33
aungpor <napat.patc@ku.th>
2024-03-23 17:13:47 +0700

    search update

--- src/app/search/[slug]/page.tsx
+++ src/app/search/[slug]/page.tsx
@@ -5,23 +5,30 @@ import RestaurantCard from "@/components/cards/Restaurantcard";
 import ToggleTest from "@/components/AppBarToggle";
 import { storeTemp } from "@/data/store";
 import FilterBar from "@/components/FilterBar";
+import Accordion from '@mui/material/Accordion';
+import AccordionActions from '@mui/material/AccordionActions';
+import AccordionSummary from '@mui/material/AccordionSummary';
+import AccordionDetails from '@mui/material/AccordionDetails';
+import ExpandMoreIcon from '@mui/icons-material/ExpandMore';
+import {
+    Box,
+    Grid,
+    MenuItem,
+    Select,
+} from "@mui/material";
 
 export default function SearchPage() {
     return (
         <>
-            <div className="py-24 mx-8 md:mx-24 lg:mx-32">
-                <form className="flex items-center justify-center ">
-                    <SearchBar placeholder={"Search Keyword..."} />
-                </form>
+            <div className="pt-24 mx-8 md:mx-24 lg:mx-32">
+                <SearchBar placeholder={"Search Keyword..."} />
             </div>
-            <section className="flex  ">
-                <div className="flex flex-col mx-auto">
-                    <Filter />
-                </div>
-                <section className="w-fit flex mr-auto grid grid-cols-1 lg:grid-cols-3 md:grid-cols-2 justify-items-center justify-center gap-y-20 gap-x-14 mt-10 mb-5">
-                    <RestaurantCard />
-                </section>
-            </section>
+
+            <Filter />
+            
+            <div className="w-fit flex mr-auto grid grid-cols-1 lg:grid-cols-3 md:grid-cols-2 justify-items-center justify-center gap-y-20 gap-x-14 mt-10 mb-5">
+                <RestaurantCard />
+            </div>
         </>
     );
 }
--- src/components/cards/Restaurantcard.tsx
+++ src/components/cards/Restaurantcard.tsx
@@ -115,7 +115,7 @@ export default function Restaurantcard() {
   };
 
   useEffect(() => {
-    fetchData();
+    // fetchData();
   }, []);
 
   return (
